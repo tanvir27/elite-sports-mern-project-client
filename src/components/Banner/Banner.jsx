61c77982fe5a37acef00b72b766eea696981ba29
Tanvir Hasan <87339504+tanvir27@users.noreply.github.com>
2023-06-12 12:51:16 +0600

    banner section added

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,26 +1,40 @@
 import React, { useRef, useState } from "react";
-// Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-import image from '../../assets/football-trainer-teaching-his-pupils.jpg'
-// Import Swiper styles
+import { Autoplay, Pagination, Navigation } from "swiper";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-
-import { Autoplay, Pagination, Navigation } from "swiper";
-
 import "./banner.css";
 
 const Banner = () => {
- const progressCircle = useRef(null);
- const progressContent = useRef(null);
- const onAutoplayTimeLeft = (s, time, progress) => {
-   progressCircle.current.style.setProperty("--progress", 1 - progress);
-   progressContent.current.textContent = `${Math.ceil(time / 1000)}s`;
- };
+  const progressCircle = useRef(null);
+  const progressContent = useRef(null);
+
+  const onAutoplayTimeLeft = (s, time, progress) => {
+    progressCircle.current.style.setProperty("--progress", 1 - progress);
+    progressContent.current.textContent = `${Math.ceil(time / 1000)}s`;
+  };
+
   return (
-    <div className="">
-      <>
+    <div className="lg:flex lg:items-center  ">
+      <div className="">
+        <h1 className="text-4xl font-bold mb-4 text-center">
+          Welcome to <span className="text-amber-500">Elite Sports</span>
+        </h1>
+
+        <h3></h3>
+        <p className="text-xl text-justify lg:px-10">
+          Unlock your potential with our top-notch sports programs. Join us to
+          learn from experienced instructors, build your skills in your favorite
+          sports activities. Limited seats available - enroll today!
+        </p>
+        <div className="text-center mb-6">
+          <button className="btn btn-outline border-0 border-b-4 mt-4 bg-slate-900 text-white">
+            Enroll Now
+          </button>
+        </div>
+      </div>
+      <div className="lg:w-1/2 lg:pt-10 lg:pb-16 rounded">
         <Swiper
           spaceBetween={30}
           centeredSlides={true}
@@ -34,67 +48,36 @@ const Banner = () => {
           navigation={true}
           modules={[Autoplay, Pagination, Navigation]}
           onAutoplayTimeLeft={onAutoplayTimeLeft}
-          className="mySwiper"
+          className="w-full"
         >
           <SwiperSlide>
-            <div className="relative ">
-              <img className="rounded " src={image} alt="" />
-              <div className="absolute top-0 left-0 right-0 bottom-0 flex flex-col justify-center items-center">
-                <h2 className=" text-3xl font-bold mb-4">
-                  "Limited Seats Available - Enroll Today!"
-                </h2>
-                <p className=" text-lg">
-                  {" "}
-                  Don't miss out on this opportunity to develop your skills,
-                  forge lasting friendships, and experience the thrill of
-                  sports.
-                </p>
-                <p>
-                  Enroll today and take the first step towards reaching your
-                  full athletic potential!
-                </p>
-              </div>
-            </div>
+            <img
+              className="rounded"
+              src="https://i.ibb.co/HYBYLcH/side-view-boy-training-gym.jpg"
+              alt=""
+            />
           </SwiperSlide>
           <SwiperSlide>
-            <div className="relative ">
-              <img className="rounded " src={image} alt="" />
-              <div className="absolute top-0 left-0 right-0 bottom-0 flex flex-col justify-center items-center">
-                <h2 className=" text-3xl font-bold mb-4">
-                  "Limited Seats Available - Enroll Today!"
-                </h2>
-                <p className=" text-lg">
-                  {" "}
-                  Don't miss out on this opportunity to develop your skills,
-                  forge lasting friendships, and experience the thrill of
-                  sports.
-                </p>
-                <p>
-                  Enroll today and take the first step towards reaching your
-                  full athletic potential!
-                </p>
-              </div>
-            </div>
+            <img
+              className="rounded"
+              src="https://i.ibb.co/SBcqKnn/man-playing-padel.jpg"
+              alt=""
+            />
+          </SwiperSlide>
+
+          <SwiperSlide>
+            <img
+              className="rounded"
+              src="https://i.ibb.co/Vj0szZ8/two-judokas-fighters-fighting-men.jpg"
+              alt=""
+            />
           </SwiperSlide>
           <SwiperSlide>
-            <div className="relative ">
-              <img className="rounded " src={image} alt="" />
-              <div className="absolute top-0 left-0 right-0 bottom-0 flex flex-col justify-center items-center">
-                <h2 className=" text-3xl font-bold mb-4">
-                  "Limited Seats Available - Enroll Today!"
-                </h2>
-                <p className=" text-lg">
-                  {" "}
-                  Don't miss out on this opportunity to develop your skills,
-                  forge lasting friendships, and experience the thrill of
-                  sports.
-                </p>
-                <p>
-                  Enroll today and take the first step towards reaching your
-                  full athletic potential!
-                </p>
-              </div>
-            </div>
+            <img
+              className="rounded"
+              src="https://i.ibb.co/10Ytxds/football-trainer-teaching-his-pupils-1.jpg"
+              alt=""
+            />
           </SwiperSlide>
 
           <div className="autoplay-progress" slot="container-end">
@@ -104,7 +87,7 @@ const Banner = () => {
             <span ref={progressContent}></span>
           </div>
         </Swiper>
-      </>
+      </div>
     </div>
   );
 };
